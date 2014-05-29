17573587063535346f3f4080c11419ed9683a069
Joshua Smith <joshuawordsmith@gmail.com>
2014-05-29 17:27:29 -0400

    added submit functionality

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,21 +1,62 @@
+'use strict';
 //initializes messages variable
-var messages;
+var serverData;
+//varialble for current user
+var user = 'Joshua';
+//template for incoming chat data
+var chatlog = _.template($('.chat-message').text());
 
-var chatlog = new _.template$('.blah');
-//gets the json from tiny serve, assigns that object to data
-$.getJSON('http://tiny-pizza-server.herokuapp.com/collections/chat-messages', function( data ) {
-  //gives data obj to messages variable
-  messages = data;
+//grabs array of data and appends it to chatlog
+function getChat(data) {
+	//counts from zero - last index of data
+	for (var i = 0; i < data.length - 1; i += 1) {
 
-
-  //counts from zero - last index of data
-  for (var i = 0; i < data.length - 1; i += 1) {
-  
-  		if (data[i].message) {
-  			$('.chat-messages').append(data[i].message);
-  		 // console.log(data[i].message);
-		  }
+		//checks to make sure message property exists
+		if (data[i].message) {
+			//assigns variable to the data through the template
+			var rendered = chatlog(data[i]);
+			//appends the chatlog div with the messages
+			$('.chat-log').append(rendered);
+		}
 	}
-  // $.each( data, function( key, val ) {
-  //   items.push( "<li id='" + key + "'>" + val + "</li>" );
-  });
+}
+
+//gets the json from tiny server, assigns that object to data
+$.getJSON('http://tiny-pizza-server.herokuapp.com/collections/chat-messages').done(function(data) {
+	//gives data obj to messages variable
+	serverData = data;
+	//calls getChat and passes in the array from tiny server
+	getChat(data);
+
+});
+
+//constructor that makes a message object to be sent
+function Message(user, message, time) {
+	this.user = '';
+	this.message = '';
+	this.time = '';
+	this.meta = 'WHOA';
+}
+
+function updateChat(info) {
+	$.post('http://tiny-pizza-server.herokuapp.com/collections/chat-messages', {
+		name: info.user,
+		message: info.message,
+		time: info.time,
+		meta: this.meta
+	});
+};
+
+//when button is clicked it creates an instance of Message
+$('.submit').click(function() {
+	//sets whatever is in the input feild and sets it to message
+	var message = $('.message').val();
+	//sets curren epoch time as time
+	var time = date.now();
+	//creates instance called outgoing
+	var outgoing = new Message(user, message, time);
+
+	//sends the data outgoing has to the server
+	updateChat(outgoing);
+
+});
