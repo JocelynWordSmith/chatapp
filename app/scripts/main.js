96a15ab072f2aa09b3ffef6c04213e25167812d4
Joshua Smith <joshuawordsmith@gmail.com>
2014-05-29 18:04:56 -0400

    clear field

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -7,7 +7,9 @@ var user = 'Joshua';
 var chatlog = _.template($('.chat-message').text());
 
 //grabs array of data and appends it to chatlog
-function getChat(data) {
+function renderChat(data) {
+	//clears chat of existing content
+	$('.chat-log').empty();
 	//counts from zero - last index of data
 	for (var i = 0; i < data.length - 1; i += 1) {
 
@@ -21,42 +23,43 @@ function getChat(data) {
 	}
 }
 
-//gets the json from tiny server, assigns that object to data
-$.getJSON('http://tiny-pizza-server.herokuapp.com/collections/chat-messages').done(function(data) {
-	//gives data obj to messages variable
-	serverData = data;
-	//calls getChat and passes in the array from tiny server
-	getChat(data);
+function loadChat () {
+	//gets the json from tiny server, assigns that object to data
+	$.getJSON('http://tiny-pizza-server.herokuapp.com/collections/chat-messages').done(function(data) {
+		//gives data obj to messages variable
+		serverData = data;
+		//calls getChat and passes in the array from tiny server
+		renderChat(data);
 
-});
+	})
+};
 
 //constructor that makes a message object to be sent
 function Message(user, message, time) {
-	this.user = '';
-	this.message = '';
-	this.time = '';
+	this.user = user || '';
+	this.message = message || '';
+	this.time = time || '';
 	this.meta = 'WHOA';
 }
 
 function updateChat(info) {
-	$.post('http://tiny-pizza-server.herokuapp.com/collections/chat-messages', {
-		name: info.user,
-		message: info.message,
-		time: info.time,
-		meta: this.meta
-	});
+	$.post('http://tiny-pizza-server.herokuapp.com/collections/chat-messages', info);
 };
+
 
 //when button is clicked it creates an instance of Message
 $('.submit').click(function() {
 	//sets whatever is in the input feild and sets it to message
 	var message = $('.message').val();
 	//sets curren epoch time as time
-	var time = date.now();
+	var time = Date.now();
 	//creates instance called outgoing
 	var outgoing = new Message(user, message, time);
 
 	//sends the data outgoing has to the server
 	updateChat(outgoing);
-
+	var form = document.getElementById('.message');
+	$('.message').form.reset();
 });
+
+setInterval(loadChat, 1000);
